0000000170bc299b773c48586615897b3478708d
Incests <42FF2AAA39EC4345FA7A4572EA49E359EA5AD42C@protonmail.com>
2021-09-18 20:40:47 +0700

    Regex & Anti-Blunder

--- token-login/index.js
+++ token-login/index.js
@@ -3,20 +3,34 @@ import { webpackModules } from '@cumcord/modules'
 let unpatch
 
 export default {
-  onLoad() {
+  onLoad () {
     // TODO: Disable token warning
     // TODO: Learn react and create token box in login page
     // TODO: Learn discord modules and create account switcher plugin
+    const MFA = /mfa\.[\w-]{84}/g
+    const nMFA = /[\w-]{24}\.[\w-]{6}\.[\w-]{27}/g
     unpatch = cumcord.patcher.after('sendMessage', webpackModules.findByProps('sendMessage'), args => {
-      if (args[1].content.startsWith('!login') && args[1].content.split(' ')[1] !== undefined) {
-        console.log('Switching account...')
-        webpackModules.findByProps('setToken').setToken(args[1].content.split(' ')[1])
-        window.location.reload()
+      if (args[1].content.startsWith('!login')) {
+        const token = args[1].content.split(' ')[1]
+        console.log('owo', token)
+        if (!token) {
+          cumcord.ui.toasts.showToast({ title: 'Token not provided!', duration: 5000 })
+          args[1].content = ''
+          return
+        }
+        if (MFA.test(token) || nMFA.test(token)) {
+          console.log('Switching account...')
+          webpackModules.findByProps('setToken').setToken(token)
+          window.location.reload()
+        } else {
+          cumcord.ui.toasts.showToast({ title: 'Invalid token format!', duration: 5000 })
+          args[1].content = ''
+        }
       }
     })
   },
 
-  onUnload() {
+  onUnload () {
     unpatch()
   }
 }
